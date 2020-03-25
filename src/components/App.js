4426aed31f22ca3986022c9a6a9b0ad90016b15c
Oluwatobi Oyejide <moyejide2016@gmail.com>
2020-03-25 14:39:47 +0100

    created the courses page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import HomePage from './home/HomePage';
 import AboutPage from './about/AboutPage';
 import Header from './common/Header';
 import PageNotFound from './PageNotFound';
+import CoursesPage from './courses/CoursesPage';
+
 
 
 const App = () => {
@@ -13,6 +15,7 @@ const App = () => {
       <Switch>
         <Route path='/' exact component={HomePage}/>
         <Route path='/about' component={AboutPage}/>
+        <Route path='/courses' component={CoursesPage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
